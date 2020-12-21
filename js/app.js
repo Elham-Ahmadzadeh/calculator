53d9d5a0767de4bce3c6d6551da24cc83ef2ee56
Elham <59868567+jensengbg-elham-ahmadzadh@users.noreply.github.com>
2020-12-21 18:11:42 +0100

    js

--- js/app.js
+++ js/app.js
@@ -1,24 +1,58 @@
-
-const screen = document.querySelector('.output'),
-currentOperand = document.querySelector('.currentOperand'),
-buttons = document.querySelectorAll('.buttons'),
-deleteKey = document.querySelector('.delete'),
-keyOperators = document.querySelectorAll('.keyOperator'),
-operating = false
-operators = [ '+', '-', 'x', '/', '=', 'cl'], /* help us to build up our row of operator buttons */
-buffer = '', /* keeps the current number in memory */
-currentResult = 0, /* updated after each operation */
-numberScreen = 'screen'
-currentOperation = ''; /* this array will hold each number n operation */
-
-
-
-function clearScreen() {
-  currentOperand.innerHTML = '0';  
-}
- function isLastEnteredNum() {
-
- }
+new Vue({
+  el: '#app',
+  data() {
+    return {
+      current: '',
+      operator: null,
+      operatorClicked: false
+    }
+  },
+  methods: {
+    clear() {
+      this.current = ''
+    },
+    append(num) {
+      if (this.operatorClicked) {
+        this.current = ''
+        this.operatorClicked = false
+      }
+      this.current = `${this.current}${num}`
+      
+    },
+    dot() {
+      if (this.current.indexOf('.') === -1) {
+        this.append('.')
+      }
+    },
+    setPrevious() {
+      this.previous = this.current
+      this.operatorClicked = true
+      
+    },
+    divide() { 
+      this.operator = (a, b) => a / b
+      this.setPrevious()
+    },
+    times() {
+      this.operator = (a, b) => a * b
+       this.setPrevious()
+     },
+    add() { 
+      this.operator = (a, b) => a + b
+  this.setPrevious()
+  
+    },
+    minus() {
+      this.operator = (a, b) => a - b
+  this.setPrevious()
+    },
+    equal() {
+      this.current = `${this.operator(parseFloat(this.previous), parseFloat(this.current))}`
+      this.previous = null
+    }
+    
+  },
+})
 
 
 
